fd319bbcd3b8017436ec17f389bbb7f6fd44f109
Katerina Latysheva <katenych@MBP-Katerina.attlocal.net>
2022-11-26 17:09:51 -0800

    added .gitignore

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+//git init
+//git status
+//git add .
+//git commit -m " message "
